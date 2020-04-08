4c26c60243e480426485e7b2583ed624c7659cee
CiaraMaria Proia <proiacm@gmail.com>
2020-04-08 19:04:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,8 @@ require 'pry'
 class Song
   
  extend Memorable::ClassMethods
- extend Findable::ClassMethods 
+ extend Findable::ClassMethods
+ include Paramable::InstanceMethods
  
   attr_accessor :name
   attr_reader :artist
